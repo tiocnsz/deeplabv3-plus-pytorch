5f8c92c837f9b724de63d235831e3c2aead06976
bbloodtech <square156756@163.com>
2023-01-06 16:11:06 +0800

    update for gray_wood

--- voc_annotation.py
+++ voc_annotation.py
@@ -22,8 +22,8 @@ VOCdevkit_path      = 'VOCdevkit'
 if __name__ == "__main__":
     random.seed(0)
     print("Generate txt in ImageSets.")
-    segfilepath     = os.path.join(VOCdevkit_path, 'VOC2007/SegmentationClass')
-    saveBasePath    = os.path.join(VOCdevkit_path, 'VOC2007/ImageSets/Segmentation')
+    segfilepath     = os.path.join(VOCdevkit_path, 'gray_wood/SegmentationClass')
+    saveBasePath    = os.path.join(VOCdevkit_path, 'gray_wood/ImageSets/Segmentation')
     
     temp_seg = os.listdir(segfilepath)
     total_seg = []
